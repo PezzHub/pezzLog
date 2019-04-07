0e702a9c8252ed7234a62c552d5e9377d79fc90a
pezz5224 <pezz5224@gmail.com>
2019-04-07 22:11:57 +0900

    (some message stating you added .idea to ignored entries)

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,23 @@ module.exports = {
     title: `PezzLog`,
   },
   plugins: [
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+            },
+          },
+        ],
+      },
+    },
     `gatsby-plugin-sass`,
     `gatsby-plugin-lodash`,
     {
